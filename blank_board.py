ed3dcce9d8aa5cc3872baed6ee63b8b2246d3957
ethanpeacockaol <ethanpeacock@aol.com>
2025-03-28 23:39:37 -0500

    hey so to print quickly in one line is in gemini_python Numbers Guessing Game add it

--- blank_board.py
+++ blank_board.py
@@ -14,3 +14,20 @@
 
 
 # TODO: add todos to blank_board this should create a board  of ~ pixels padded by the todo in question and when the window is resized adjust frame
+
+
+
+import os
+
+rowz, colz = os.popen('stty size', 'r').read().split()
+rows = int(rowz)
+cols = int(colz)
+
+print(rows, cols)
+
+import numpy as np
+
+x = np.zeros((rows, cols))
+
+
+# TODO: add quick print from gemini_python NumbersGuessingGame main.py before i delete it
