63f35024e0120d3614874da476f4ea62d38f17b6
ethanpeacockaol <ethanpeacock@aol.com>
2025-03-28 23:05:18 -0500

    add todos to blank board

---
+++ blank_board.py
@@ -0,0 +1,16 @@
+# TODO: frame should  have 
+# __
+#|    at left corner top
+
+#                          __   
+#     at right corner top    |
+
+# |       at bottom left corner
+#  ---
+
+#         at bottom right corner      | 
+#                                  ---
+
+
+
+# TODO: add todos to blank_board this should create a board  of ~ pixels padded by the todo in question and when the window is resized adjust frame
