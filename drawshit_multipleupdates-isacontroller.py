aa04103d417ddf8b9c905169214f91abd8a2e891
ethan <ethanpeacock@aol.com>
2025-07-08 14:06:30 -0500

    we have to program an animator and a controller

---
+++ drawshit_multipleupdates-isacontroller.py
@@ -0,0 +1,137 @@
+# hey read drawyesnononononoyes or whatever right
+
+
+# so then how do we move and fire?
+
+# what?
+
+# so we take chartacter inputs still but the controller needs to send out right so we have to program a wirter that is the controller
+
+# lets say we are playing paintball program
+# what can our player do
+# they can move aim and shoot just that its a simple one right
+# okay so we ca do the int update right
+
+thats like 
+0
+0
+0
+0
+0
+0
+0
+0
+0
+HEY PLAYER HIT RIGHT ARROW, WELL WE MOVE THEM X TO THE RIGHT
+BUT HERE WE ARE JUST USING INTEGER INCREMENTS AND DECREMENTS 
+YOU CAN STILL THINK OF THIS AS MOTION
+IF YOU VIEW A DOT ON AN INTEGER INCREMENTED NUMBERLINE IS IT | IS IT - IDK
+
+but hey they hit the right arrow
+
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1 
+hey right arrow hit again
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+hey right arrow is being held
+thats the same as 
+oh fuck they still pressing right arrow
+so
+right arrow
+right arrow
+right arrow
+right arrow
+right arrow
+right arrow
+right arrow
+right arrow
+right arrow
+right arrow
+12
+12
+12
+12
+12
+12
+12
+12
+12
+12
+12
+12
+12
+12
+12
+12
+right?
+
+
+okay so how do we move and jump?
+
+well the controller needs a dictionary and key or a key and value wtf its called
+right
+
+so we set our countroller up to do input logic sanitization
+
+okay
+
+if the input catcher finds that an assorted amount of buttons are pressed 
+we concatenate predefined definitions and verbs or whatever to indicate what to do 
+right 
+
+so now the user input is like some string we define and then look up its sequential actions for the next frame update
+whats that mean
+well lets say they hold down move right and hit space bar to fire paintball gun
+lets assume paintball gun is autoaiming but you cannot shoot through obstacles right
+
+okay so here you might would say
+
+from your controller program thats doing logic combinations of sanitizing input of keyboard using time and the whatever from drawyesno
+
+you could send to the game program like a string or something that says 
+
+move right then fire 
+
+
+idk right
+
+output = 'rightFire'
+
+compared to what??
+okay shit they can aim i forgot
+
+okay so how do they aim
+
+well wasd for player movement 
+
+up down left right for aim
+
+space bar to shoot
+
+idk how many possible combinations there are of doing all
+but in the possibilites the differentations cause different physicallites of in the moment decision right?
+so the string identifiers should be hooked up tologic that tells the animation and logic engine what order to draw things in given thaqt 
+there is a time requirement of only deviate this x amount of time to the frame drawing before you just draw
+only wait an x amount of time for user input (even smaller possibly? possibly not at all actually idk) but only wait
+only wait an x amount of time for input before you draw too
+idk save it and make it later if it works
+
