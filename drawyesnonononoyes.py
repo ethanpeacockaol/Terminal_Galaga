fb50e39a85c5bffc9fa1bb3119a4e23ce670a230
ethan <ethanpeacock@aol.com>
2025-07-08 13:56:18 -0500

    hey here is the running idea for player updates in the animation

---
+++ drawyesnonononoyes.py
@@ -0,0 +1,120 @@
+# what we wann do is start printing a number input? no keep printing
+# input? yes if the user types 
+
+# so fuck i dont have enough time to copy over the gemini answer to user input without enter but they say
+# use the termios and tty librarys to set raw mode? and then take only single character input
+
+# so possibly we use the time library to say hey have they entered a single charaacer? no well shit its been like 0.003m? so fuck we gotta draw the next screen this equals hey dont increment hte number 
+
+# oh they did push a character
+# well incrememnt the number with like the right arrow  or something
+# decrement with left arrow or something like this should be the ouput right
+
+
+
+# we are starting our program here:
+________________
+
+0
+0
+0
+0
+0
+0
+USER HITS RIGHT ARROW
+1
+1
+1
+1
+1
+1
+USER HITS RIGHT ARROW
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+2
+USER HITS LEFT ARROW
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+1
+USER HITS RIGHT ARROW
+USER HITS RIGHT ARROW
+USER HITS RIGHT ARROW
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+4
+USER HITS LEFT ARROW
+3
+3
+3
+3
+3
+3
+3
+3
+3
+USER HITS LEFT ARROW 
+USER HITS LEFT ARROW 
+USER HITS LEFT ARROW 
+USER HITS LEFT ARROW 
+USER HITS LEFT ARROW 
+USER HITS LEFT ARROW 
+USER HITS LEFT ARROW 
+10
+10
+10
+10
+10
+10
+10
+10
+10
+10
+USER HITS QUIT
+thank you bye
+
